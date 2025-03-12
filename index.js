fb6ef00c7df928c737ad4750d3e1f4fe55a5a4c5
rajeshrautFSD <rajeshraut576@gmail.com>
2025-03-12 17:52:58 +0530

    final ans

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+
+let nums = [-2,-1,-1,1,2,3]
+
+
 function maximumCount(nums) {
     let negCount = 0, posCount = 0;
 
-    // Loop through the array to count negative and positive numbers
+
     for (let num of nums) {
         if (num < 0) {
             negCount++;
@@ -13,7 +17,4 @@ function maximumCount(nums) {
     return Math.max(negCount, posCount);
 }
 
-// Test cases
-console.log(maximumCount([-2, -1, -1, 1, 2, 3])); // Output: 3
-console.log(maximumCount([-3, -2, -1, 0, 0, 1, 2])); // Output: 3
-console.log(maximumCount([5, 20, 66, 1314])); // Output: 4
+console.log(maximumCount(nums));
