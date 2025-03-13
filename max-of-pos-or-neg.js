d47d8e750a3adae032874f48ad45936895865577
rajeshrautFSD <rajeshraut576@gmail.com>
2025-03-13 19:25:07 +0530

    lets go

--- max-of-pos-or-neg.js
+++ max-of-pos-or-neg.js
@@ -5,16 +5,16 @@ let nums = [-2,-1,-1,1,2,3]
 function maximumCount(nums) {
     let negCount = 0, posCount = 0;
 
+ 
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i]; 
 
-    for (let num of nums) {
-        if (num < 0) {
-            negCount++;
-        } else if (num > 0) {
-            posCount++;
-        }
+    if (num < 0) {
+        negCount++;
+    } else if (num > 0) {
+        posCount++;
     }
-
-    return Math.max(negCount, posCount);
 }
-
+return Math.max(negCount, posCount);
+}
 console.log(maximumCount(nums));
